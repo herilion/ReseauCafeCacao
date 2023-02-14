8912144611419fb4d2e1600cb865b42d1dd3625d
alexissengangabo07 <alexxngabo@gmail.com>
2023-02-14 18:08:12 +0200

    feat: add slider

--- frontend/src/composants/slider/Slider.jsx
+++ frontend/src/composants/slider/Slider.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Carousel } from 'react-responsive-carousel';
 
 export default function Slider() {
     return (
-        <div>Slider</div>
+        <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false} showStatus={false}>
+            <div>
+                <img src="/images/slide1.png" />
+                <p className="legend">Legend 1</p>
+            </div>
+            <div>
+                <img src="/images/logo.png" />
+                <div>
+                <p className>Legend 2</p>
+                 <button>OK</button>
+                </div>
+            </div>
+        </Carousel>
     )
 }
