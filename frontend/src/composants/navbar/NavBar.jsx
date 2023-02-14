d96038ec133b4175198eabc0efefd77c574b5e0b
alexissengangabo07 <alexxngabo@gmail.com>
2023-02-14 16:20:22 +0200

    style: navbar menu

---
+++ frontend/src/composants/navbar/NavBar.jsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import {
+    TfiFacebook,
+    TfiTwitterAlt
+} from 'react-icons/tfi';
+import { IoLogoYoutube } from 'react-icons/io';
+import { GrMail } from 'react-icons/gr';
+import { BiSearch } from 'react-icons/bi';
+import './style.css';
+
+const NavBar = () => {
+    return (
+        <>
+            <header className='header'>
+                <div>
+                    <h6>Réseaux Café Cacao</h6>
+                </div>
+                <div className='top-icons-container'>
+                    <h6>Réseaux Café Cacao</h6>
+                    <Link to='https://facebook.com' target="_blank"><TfiFacebook /></Link>
+                    <Link to='https://twitter.com' target="_blank"><TfiTwitterAlt /></Link>
+                    <Link to='https://youtube.com' target="_blank"><IoLogoYoutube /></Link>
+                    <Link to='https://gmail.com' target="_blank"><GrMail /></Link>
+                </div>
+            </header>
+            <nav className='navbar'>
+                <div className="navbar-logo">
+                    <Link to="/">
+                        <img src="/images/logo.png" alt="website logo" style={{ width: 90 }} />
+                    </Link>
+                </div>
+                <div className="navbar-menu">
+                    <ul className='navbar-links'>
+                        <li><Link to="/ok">ACCEUIL</Link></li>
+                        <li><Link to="/ok">PROGRAMMES</Link></li>
+                        <li><Link to="/ok">NOS MEMBRES</Link></li>
+                        <li><Link to="/ok">NOUVELLES</Link></li>
+                        <li><Link to="/ok">RESSOURCES</Link></li>
+                        <li><Link to="/ok">A PROPOS</Link></li>
+                        <li><Link to="/ok">CONTACT</Link></li>
+                    </ul>
+                    <div className='top-search-icon'>
+                        <BiSearch />
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar;
