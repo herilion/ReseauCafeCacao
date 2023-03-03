018db77bb4be2ff52cbb72a2f5b890088e031d1a
herilion <heritierlionge@yahoo.com>
2023-03-03 16:44:16 +0200

    update carossel

--- frontend/src/composants/slider/Slider.jsx
+++ frontend/src/composants/slider/Slider.jsx
@@ -1,17 +1,18 @@
 import { Carousel } from 'react-responsive-carousel';
+import './slider.css'
 
 export default function Slider() {
     return (
         <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false} showStatus={false}>
             <div>
-                <img src="/images/slide1.png" />
+                <img src="/images/slide1.png" className='sl' />
                 <p className="legend">Legend 1</p>
             </div>
             <div>
-                <img src="/images/logo.png" />
+                <img src="/images/logo.png" className='sl' />
                 <div>
-                <p className>Legend 2</p>
-                 <button>OK</button>
+                    <p className>Legend 2</p>
+                    <button>OK</button>
                 </div>
             </div>
         </Carousel>
